59628248b8ba9102717e3586f8d4df9b6dc60de3
investorVOU <omahlay917@gmail.com>
2026-02-20 21:03:14 +0100

    feat: Update README with game details and new features; add node polyfills to Vite config and package.json

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -11,6 +12,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [
+        nodePolyfills({ include: ['buffer', 'process', 'util', 'stream'] }),
         react(),
         VitePWA({
           registerType: 'autoUpdate',
